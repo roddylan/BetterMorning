87d499f935de1cd8e56eada1dee24bdcc9b0a0f2
Roderick Lan <rodericklan03@gmail.com>
2024-01-03 00:11:53 -0700

    class def

--- engine/mood.py
+++ engine/mood.py
@@ -1,4 +1,7 @@
-class mood:
-    def __init__(self):
-        pass
+class Mood:
+    def __init__(self, label):
+        self.label = label
+    
+    def __str__(self):
+        return f"< Mood: {self.label} >"
     
--- engine/routine.py
+++ engine/routine.py
@@ -1,5 +1,13 @@
-class routine:
+from mood import Mood
+
+class DefaultRoutine:
     def __init__(self):
         pass
 
-    
+
+class Routine(DefaultRoutine):
+    def __init__(self, mood: Mood):
+        self.mood = mood
+
+    def __str__(self):
+        return f"< Routine: {self.mood.label} >"
