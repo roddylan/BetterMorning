bba2a3dc58db9df7c0c2b959f000892c2f6978dc
Roderick Lan <rodericklan03@gmail.com>
2024-01-06 23:09:59 -0700

    model fix (maybe)

--- model/model.py
+++ model/model.py
@@ -4,7 +4,6 @@ def run(img):
     model = YOLO("training/runs/detect/train/weights/best.pt")
     result = model(img)[0]
     data = result.boxes.data
-    print(data[0])
     # print(data.size())
     if list(data.size())[0] != 0 or not bool(data.any()): #TODO: doesnt always catch bad results
         return 0
